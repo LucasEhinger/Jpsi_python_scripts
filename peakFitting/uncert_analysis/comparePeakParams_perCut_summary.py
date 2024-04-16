af553f1b9a0dd58613cc5bf6e455aea50c4caf9a
lucasehinger <lucasehigner@gmail.com>
2024-04-16 16:00:42 -0400

    Latest fit functions

--- peakFitting/uncert_analysis/comparePeakParams_perCut_summary.py
+++ peakFitting/uncert_analysis/comparePeakParams_perCut_summary.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pystyle
 from pystyle.annotate import placeText
 plt.style.use("SRC_CT_presentation")
-Egamma="8p2_9p5"
+Egamma="7_11"
 
 df_stdev_D =pd.read_csv(f"../../../files/figs/peakFits/uncert/5 params/Egamma_{Egamma}/stdev_means_D.csv",header=None)
 df_stdev_He =pd.read_csv(f"../../../files/figs/peakFits/uncert/5 params/Egamma_{Egamma}/stdev_means_He.csv",header=None)
@@ -20,10 +20,10 @@ y2=np.array([df_uncert_D[0][2],df_uncert_He[0][2],df_uncert_C[0][2]])*100
 y3=np.array([df_uncert_D[0][3],df_uncert_He[0][3],df_uncert_C[0][3]])*100
 
 x=[0,1,2]
-plt.bar(x,y0,color='b',label='preB')
-plt.bar(x,y1,bottom=y0,color='orange',label='sigma_min')
-plt.bar(x,y2,bottom=y0+y1,color='g',label='sigma_max')
-plt.bar(x,y3,bottom=y0+y1+y2,color='r',label="E_miss")
+plt.bar(x,y0,color='b',label=r'$E_{preshower}$')
+plt.bar(x,y1,bottom=y0,color='orange',label='p/E lower')
+plt.bar(x,y2,bottom=y0+y1,color='g',label='p/E upper')
+plt.bar(x,y3,bottom=y0+y1+y2,color='r',label=r'$E_{miss}$')
 plt.xticks(x,["D","He","C"])
 plt.legend(fontsize=10)
 plt.ylabel('Uncertainty (%)')
